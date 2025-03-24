265ab8290d308b96d21388c5accc2101d111da6d
nmin11 <nmin1124@gmail.com>
2025-03-24 20:48:56 +0900

    feat: implement get user point history

--- src/main/kotlin/io/hhplus/tdd/point/PointController.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointController.kt
@@ -28,7 +28,7 @@ class PointController(
     fun history(
         @PathVariable id: Long,
     ): List<PointHistory> {
-        return emptyList()
+        return pointService.getUserPointHistory(id)
     }
 
     /**
--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -1,13 +1,19 @@
 package io.hhplus.tdd.point
 
+import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import org.springframework.stereotype.Service
 
 @Service
 class PointService(
-    private val userPointTable: UserPointTable
+    private val userPointTable: UserPointTable,
+    private val pointHistoryTable: PointHistoryTable
 ) {
     fun getUserPoint(id: Long): UserPoint {
         return userPointTable.selectById(id)
+    }
+
+    fun getUserPointHistory(id: Long): List<PointHistory> {
+        return pointHistoryTable.selectAllByUserId(id)
     }
 }
