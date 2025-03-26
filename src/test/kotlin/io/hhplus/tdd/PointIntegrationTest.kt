ed4bafd0fad3d812fe30261c042d1ea61ad7608f
nmin11 <nmin1124@gmail.com>
2025-03-26 23:23:37 +0900

    test: case of points exceeding 1 million by concurrent requests

--- src/test/kotlin/io/hhplus/tdd/PointIntegrationTest.kt
+++ src/test/kotlin/io/hhplus/tdd/PointIntegrationTest.kt
@@ -3,14 +3,20 @@ package io.hhplus.tdd
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import io.hhplus.tdd.point.TransactionType
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.mock.web.MockHttpServletResponse
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -61,5 +67,50 @@ class PointIntegrationTest {
             .perform(get("/point/$nonExistenceId/histories"))
             .andExpect(status().isOk)
             .andExpect(jsonPath("$.length()").value(0))
+    }
+
+    @Test
+    fun `95만_포인트를_가진_유저에게_3만_포인트_충전_요청이_2개가_동시에_들어오면_한_요청은_실패`() {
+        // given
+        val userId = 1L
+        userPointTable.insertOrUpdate(userId, 950_000L)
+        val chargeAmount = 30000L
+        val latch = CountDownLatch(2)
+        val executor = Executors.newFixedThreadPool(2)
+        val results = mutableListOf<MockHttpServletResponse>()
+
+        // when
+        repeat(2) {
+            executor.submit {
+                try {
+                    val result = mockMvc
+                        .perform(
+                            patch("/point/$userId/charge")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(chargeAmount.toString())
+                        ).andReturn().response
+                    results.add(result)
+                } catch (e: Exception) {
+                    println("예외 발생: ${e.message}")
+                } finally {
+                    latch.countDown()
+                }
+            }
+        }
+
+        latch.await()
+
+        val successCount = results.count { it.status == 200 }
+        val failCount = results.count {
+            it.status == 500 &&
+            String(it.contentAsByteArray, Charsets.UTF_8)
+                .contains("포인트는 1,000,000원을 초과할 수 없습니다.")
+        }
+
+        assertThat(successCount).isEqualTo(1)
+        assertThat(failCount).isEqualTo(1)
+
+        val userPoint = userPointTable.selectById(1L)
+        assertThat(userPoint.point).isEqualTo(980_000L)
     }
 }
